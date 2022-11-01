1cbe41eab7b2e0b48112a1372ecc670a86699212
Frizto <frizto@gmail.com>
2022-11-01 11:07:10 -0300

    changing the matrix to chess position chars

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabletop;
+using chess;
 
 namespace Chess_Console
 {
@@ -7,21 +8,10 @@ namespace Chess_Console
     {
         static void Main(string[] args)
         {
-            try
-            {
+            ChessPosition pos = new ChessPosition('c', 7);
 
-                Tabletop tab = new Tabletop(8, 8);
-
-                tab.setPiece(new Tower(tab, Color.Black), new Position(0, 0));
-                tab.setPiece(new Tower(tab, Color.Black), new Position(1, 3));
-                tab.setPiece(new King(tab, Color.Black), new Position(0, 2));
-
-                Screen.printTabletop(tab);
-            }
-            catch (TabletopException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosition());
 
             Console.ReadLine();
         }
---
+++ chess/ChessPosition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabletop;
+
+namespace chess
+{
+    class ChessPosition
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public ChessPosition(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - linha, coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
